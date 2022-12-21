5dec71a643c8cf74391679bdc0f71a5eed9f99f0
cevik <suleymancevik64@gmail.com>
2022-12-21 21:52:17 +0300

    cevik

---
+++ src/test/java/stepDefinitions/LoginUserInCorrect.java
@@ -0,0 +1,23 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import pages.AutomationPage;
+
+public class LoginUserInCorrect {
+
+    AutomationPage page = new AutomationPage();
+    @Then("Kullanici Yanlis e-posta adresini ve şifreyi girer")
+    public void kullaniciYanlisEPostaAdresiniVeŞifreyiGirer() {
+
+        page.emailBoxLogin.sendKeys("mahmut@gmail.com");
+        page.passwordBoxLogin.sendKeys("qwert1233.");
+    }
+
+    @And("Kullanici Your email or password is incorrect!'in gorundugunu dogrular")
+    public void kullaniciYourEmailOrPasswordIsIncorrectInGorundugunuDogrular() {
+
+        Assert.assertTrue(page.yourEmailOrPasswordIsIncorrect.isDisplayed());
+    }
+}
